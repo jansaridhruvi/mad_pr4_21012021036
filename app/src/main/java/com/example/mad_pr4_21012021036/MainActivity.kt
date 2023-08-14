6c7bbf1a5c944d32eb4132eb731ccea55a01ed09
jansaridhruvi <dhruvijansari21@gnu.ac.in>
2023-08-14 13:20:08 +0530

    p4

--- app/src/main/java/com/example/mad_pr4_21012021036/MainActivity.kt
+++ app/src/main/java/com/example/mad_pr4_21012021036/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.mad_pr4_21012021036
-
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       
+        var l:Button=findViewById(R.id.login);
+        var s:Button=findViewById(R.id.signup);
+        s.setOnClickListener(){
+            var i=Intent(this,regester::class.java)
+            startActivity(i);
+        }
+        l.setOnClickListener(){
+            var k=Intent(this,login::class.java)
+            startActivity(k);
+        }
     }
 }
---
+++ app/src/main/java/com/example/mad_pr4_21012021036/regester.kt
@@ -0,0 +1,11 @@
+package com.example.mad_pr4_21012021036
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class regester : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regester)
+    }
+}
