5f44ed254ee1286289bd75f2343780d65d387066
jansaridhruvi <dhruvijansari21@gnu.ac.in>
2023-10-01 21:14:40 +0530

    pr4

--- app/src/main/java/com/example/mad_pr4_21012021036/login.kt
+++ app/src/main/java/com/example/mad_pr4_21012021036/login.kt
@@ -1,11 +1,19 @@
 package com.example.mad_pr4_21012021036
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class login : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        var signup:Button=findViewById(R.id.signup);
+        signup.setOnClickListener(){
+            var intent=Intent(this,regester::class.java);
+            startActivity(intent);
+        }
     }
 }
